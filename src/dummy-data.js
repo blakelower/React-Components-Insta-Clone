b601f454a072665df1ce6d027edef1ff8a73a65e
blakelower <blakelower1999@gmail.com>
2020-01-23 16:22:34 +0700

    create another user

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,50 +1,94 @@
 const dummyData = [
-    {
+  {
+    username: "philzcoffee",
+    thumbnailUrl:
+      "https://instagram.fsjo9-1.fna.fbcdn.net/vp/d4728e507fe1eb2895fb7ca394bb6431/5E4122B1/t51.2885-15/s150x150/71118783_2434459423505735_6295784028176668008_n.jpg?_nc_ht=instagram.fsjo9-1.fna.fbcdn.net",
+    imageUrl:
+      "https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80",
+    likes: 400,
+    timestamp: "July 17th 2017, 12:42:40 pm",
+    comments: [
+      {
         username: "philzcoffee",
-        thumbnailUrl: 'https://instagram.fsjo9-1.fna.fbcdn.net/vp/d4728e507fe1eb2895fb7ca394bb6431/5E4122B1/t51.2885-15/s150x150/71118783_2434459423505735_6295784028176668008_n.jpg?_nc_ht=instagram.fsjo9-1.fna.fbcdn.net',
-        imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
-        likes: 400,
-        timestamp: "July 17th 2017, 12:42:40 pm",
-        comments: [
-            {
-                username: "philzcoffee",
-                text: "We've got more than just delicious coffees to offer at our shops!"
-            },
-            {
-                username: "biancasaurus",
-                text: "Looks delicious!"
-            },
-            {
-                username: "martinseludo",
-                text: "Can't wait to try it!"
-            }
-        ]
-    },
-    {
+        text:
+          "We've got more than just delicious coffees to offer at our shops!"
+      },
+      {
+        username: "biancasaurus",
+        text: "Looks delicious!"
+      },
+      {
+        username: "martinseludo",
+        text: "Can't wait to try it!"
+      },
+      {
+        username: "blakelower",
+        text: "yummy, cant wait!!"
+      }
+    ]
+  },
+  {
+    username: "twitch",
+    thumbnailUrl:
+      "https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg",
+    imageUrl:
+      "https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80",
+    likes: 4307,
+    timestamp: "July 15th 2017, 03:12:09 pm",
+    comments: [
+      {
         username: "twitch",
-        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
-        imageUrl: 'https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80',
-        likes: 4307,
-        timestamp: "July 15th 2017, 03:12:09 pm",
-        comments: [
-            {
-                username: "twitch",
-                text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
-            },
-            {
-                username: "michaelmarzetta",
-                text: "Omg that match was crazy"
-            },
-            {
-                username: "themexican_leprechaun",
-                text: "What a setup"
-            },
-            {
-                username: "dennis_futbol",
-                text: "It that injustice"
-            }
-        ]
-    }
+        text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
+      },
+      {
+        username: "michaelmarzetta",
+        text: "Omg that match was crazy"
+      },
+      {
+        username: "themexican_leprechaun",
+        text: "What a setup"
+      },
+      {
+        username: "dennis_futbol",
+        text: "It that injustice"
+      },
+      {
+        username: "blakelower",
+        text: "this is awesome!"
+      },
+    ]
+  },
+  {
+    username: "KylieJenner",
+    thumbnailUrl:
+      "https://banner2.cleanpng.com/20180605/ukw/kisspng-kylie-jenner-keeping-up-with-the-kardashians-kenda-jenner-5b16a14421e0e4.0382757215282097321388.jpg",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kylie-jenner-1506802477.png?resize=480:*",
+    likes: 2650000,
+    timestamp: "July 15th 2017, 03:12:09 pm",
+    comments: [
+      {
+        username: "KylieJenner",
+        text: "Feeling like a baddie🤪"
+      },
+      {
+        username: "TravisScott",
+        text: "My Baby Momma🔥"
+      },
+      {
+        username: "KimKardashian",
+        text: "Sissy!!!!👯‍♀️"
+      },
+      {
+        username: "JustinBieber",
+        text: "Hey Shawty🤷‍♂️"
+      },
+      {
+        username: "BlakeLower",
+        text: "wow so hot"
+      },
+    ]
+  }
 ];
 
 export default dummyData;
